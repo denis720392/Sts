84bcd911b622c2df31f248da6fcaeb410da905ca
Денис Турчин <denis.turchin@ya.ru>
2023-10-10 22:29:42 +0300

    3.1 - Вернул папку Product.User

---
+++ Product.User/User.cs
@@ -0,0 +1,10 @@
+﻿
+using Sts.Product.Common.Interfaces;
+using Sts.Product.Common.Utilities;
+
+namespace Sts.Product.User
+{
+    internal class User : ManagingProducts, IUsualPermission
+    {
+    }
+}
